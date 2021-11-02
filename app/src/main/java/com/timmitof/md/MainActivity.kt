bfed914c7ef54cd8a9c48b47af2c1a545b69d407
timmitof <81027444+timmitof@users.noreply.github.com>
2021-11-02 12:51:27 +0600

    Добавили окно с результатом

--- app/src/main/java/com/timmitof/md/MainActivity.kt
+++ app/src/main/java/com/timmitof/md/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.timmitof.md
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firstName: EditText
+    lateinit var age: EditText
+    lateinit var birthDate: EditText
+
+    lateinit var saveBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstName = findViewById(R.id.firstName)
+        age = findViewById(R.id.age)
+        birthDate = findViewById(R.id.birthDate)
+        saveBtn = findViewById(R.id.saveBtn)
+
+        saveBtn.setOnClickListener{
+            val name = firstName.text
+            val ageText = age.text
+            val birth = birthDate.text
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
